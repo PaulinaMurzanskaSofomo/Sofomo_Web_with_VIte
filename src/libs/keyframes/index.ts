b66cbac2b8de78256315002ca057d62ae6b9adb0
PaulinaMurzanskaSofomo <paulina.murzanska@sofomo.com>
2022-06-22 10:19:01 +0200

    Firebase first deployment

--- src/libs/keyframes/index.ts
+++
@@ -1,7 +0,0 @@
-export { infiniteLoop } from "./InfiniteLoop";
-export { currencyLoop } from "./currencyLoop";
-export { modalScaleUp, modalScaleDown } from "./modalAnimation";
-export { hideCard, showCard } from "./cardWhiteAnimation";
-export { showForm } from "./outsourceFormAnimation";
-export { menuLineFadeOut, menuLineFadeIn } from "./menuLineAnimations";
-export { navItemSlideUp, navItemSlideDown, navItemFadeIn } from "./navItemAnimations";
